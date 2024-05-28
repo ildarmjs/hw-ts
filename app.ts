c9ba3986cf939f38f82a8950d81c52b53045977e
Ildar <ildar.1795@gmail.com>
2024-05-28 10:27:47 +0400

    feat: создает и типизирует функцию, нормализующую входящие данные.

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-import { TotalPriceParams } from "./types/types";
+import { posts } from "./data/posts";
+import { TNormalizeData, TPosts, TotalPriceParams } from "./types/types";
 
 // #1 TotalPrice 
 
@@ -8,5 +9,37 @@ const totalPrice = ({ price, discount, isInstallment, months }: TotalPriceParams
 
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 })
 console.log(price)
+
+// ===============================================================================================
+
+
+// #2 NormalizeData
+
+const normalizeData = (unnormalizedData: TPosts[]): TNormalizeData => {
+	const byId: { [key: string]: TPosts } = {}
+	const allIds: string[] = []
+	unnormalizedData.forEach((post) => {
+		byId[post.id] = post
+		allIds.push(post.id)
+	});
+
+	return {
+		byId,
+		allIds,
+	}
+
+}
+
+console.log(normalizeData(posts))
+/**
+ * {
+ *    byId: {
+ *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
+ *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
+ *      ...
+ *    },
+ *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
+ * }
+ */
 
 // ===============================================================================================
